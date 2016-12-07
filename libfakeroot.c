337a7bdeed0f7151eebf769cb31566aa7a7bfd24
William Manley <will@williammanley.net>
2016-12-07 18:04:27 +0000

    Add dummy `audit_log_acct_message`
    `libaudit` is used by `chfn` to log when changing `/etc/passwd`.  This
    fails under fakeroot as we're not actually root.  Instead we replace this
    function so it succeeds.

--- libfakeroot.c
+++ libfakeroot.c
@@ -2457,3 +2457,11 @@ int sysinfo(int command, char *buf, long count)
     }
 }
 #endif
+
+int audit_log_acct_message(
+    int audit_fd, int type, const char *pgname, const char *op,
+    const char *name, unsigned int id, const char *host, const char *addr,
+    const char *tty, int result)
+{
+  return 0;
+}
